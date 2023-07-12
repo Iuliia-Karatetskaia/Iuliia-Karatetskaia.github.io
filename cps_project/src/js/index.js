bcf0ee7b3670ffce312f6fad33b76f15aa714560
Iuliia-Karatetskaia <135870022+Iuliia-Karatetskaia@users.noreply.github.com>
2023-07-12 10:34:23 +0300

    Update index.js

--- cps_project/src/js/index.js
+++ cps_project/src/js/index.js
@@ -218,7 +218,7 @@ function bodyUnlock() {
 ;(function () {
   if (!Element.prototype.closest) {
     Element.prototype.closest = function (css) {
-      var node = this
+      let node = this
       while (node) {
         if (node.matches(css)) return node
         else node = node.parentElement
