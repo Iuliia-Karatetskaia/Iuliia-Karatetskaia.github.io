5cc12d70c7ef7ade5465ce5416a9a13cc5611c96
Iuliia-Karatetskaia <135870022+Iuliia-Karatetskaia@users.noreply.github.com>
2023-07-12 10:30:01 +0300

    Update bundle.js

--- cps_project/dist/js/bundle.js
+++ cps_project/dist/js/bundle.js
@@ -10014,234 +10014,236 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
-var init = false;
-var firstSwiper;
-var secondSwiper;
-var thirdSwiper;
-var paginationStatus = document.getElementsByClassName('swiper-pagination');
+let init = false
+let firstSwiper
+let secondSwiper
+let thirdSwiper
+let paginationStatus = document.getElementsByClassName('swiper-pagination')
 
 function swiperCard() {
   if (window.innerWidth <= 768) {
     if (!init) {
-      init = true;
-
-      for (var i = 0; i < paginationStatus.length; i++) {
-        paginationStatus[i].classList.remove('pagination--hidden');
+      init = true
+      for (let i = 0; i < paginationStatus.length; i++) {
+        paginationStatus[i].classList.remove('pagination--hidden')
       }
+      firstSwiper = new Swiper('.brand-list__container', {
+        modules: [Navigation, Pagination],
+        direction: 'horizontal',
+        loop: false,
 
-      firstSwiper = new swiper__WEBPACK_IMPORTED_MODULE_4__["default"]('.brand-list__container', {
-        modules: [swiper_modules__WEBPACK_IMPORTED_MODULE_5__["Navigation"], swiper_modules__WEBPACK_IMPORTED_MODULE_5__["Pagination"]],
-        direction: 'horizontal',
-        loop: false,
         pagination: {
           el: '.swiper-pagination',
           clickable: true
         },
+
         autoHeight: true,
         slidesPerView: 'auto',
         watchOverflow: true,
         spaceBetween: 16,
         speed: 800
-      });
-      secondSwiper = new swiper__WEBPACK_IMPORTED_MODULE_4__["default"]('.equipment-list__container', {
-        modules: [swiper_modules__WEBPACK_IMPORTED_MODULE_5__["Navigation"], swiper_modules__WEBPACK_IMPORTED_MODULE_5__["Pagination"]],
+      })
+      secondSwiper = new Swiper('.equipment-list__container', {
+        modules: [Navigation, Pagination],
         direction: 'horizontal',
         loop: false,
+
         pagination: {
           el: '.swiper-pagination',
           clickable: true
         },
+
         autoHeight: true,
         slidesPerView: 'auto',
         watchOverflow: true,
         spaceBetween: 16,
         speed: 800
-      });
-      thirdSwiper = new swiper__WEBPACK_IMPORTED_MODULE_4__["default"]('.price-list__container', {
-        modules: [swiper_modules__WEBPACK_IMPORTED_MODULE_5__["Navigation"], swiper_modules__WEBPACK_IMPORTED_MODULE_5__["Pagination"]],
+      })
+      thirdSwiper = new Swiper('.price-list__container', {
+        modules: [Navigation, Pagination],
         direction: 'horizontal',
         loop: false,
+
         pagination: {
           el: '.swiper-pagination',
           clickable: true
         },
+
         autoHeight: true,
         slidesPerView: 'auto',
         watchOverflow: true,
         spaceBetween: 16,
         speed: 800
-      });
+      })
     }
   } else if (init) {
-    firstSwiper.destroy();
-    secondSwiper.destroy();
-    thirdSwiper.destroy();
-    init = false;
-
-    for (var _i = 0; _i < paginationStatus.length; _i++) {
-      paginationStatus[_i].classList.add('pagination--hidden');
+    firstSwiper.destroy()
+    secondSwiper.destroy()
+    thirdSwiper.destroy()
+    init = false
+    for (let i = 0; i < paginationStatus.length; i++) {
+      paginationStatus[i].classList.add('pagination--hidden')
     }
   }
 }
+swiperCard()
+window.addEventListener('resize', swiperCard)
 
-swiperCard();
-window.addEventListener('resize', swiperCard);
-var buttonShow = document.getElementsByClassName('show-all');
-var button = document.getElementsByClassName('show-all__button');
-var firstContainer = document.querySelector('.content__text');
-var secondContainer = document.querySelector('.brand-list__container');
-var thirdContainer = document.querySelector('.equipment-list__container');
-var arrayButton = [{
-  buttonShow: buttonShow,
-  button: button,
-  container: firstContainer,
-  condition: true
-}, {
-  buttonShow: buttonShow,
-  button: button,
-  container: secondContainer,
-  condition: true
-}, {
-  buttonShow: buttonShow,
-  button: button,
-  container: thirdContainer,
-  condition: true
-}];
+let buttonShow = document.getElementsByClassName('show-all')
+let button = document.getElementsByClassName('show-all__button')
+const firstContainer = document.querySelector('.content__text')
+const secondContainer = document.querySelector('.brand-list__container')
+const thirdContainer = document.querySelector('.equipment-list__container')
 
-for (var i = 0; i < arrayButton.length; i++) {
-  arrayButton[i].buttonShow = buttonShow[i];
-  arrayButton[i].button = button[i];
+let arrayButton = [
+  {
+    buttonShow,
+    button,
+    container: firstContainer,
+    condition: true
+  },
+
+  {
+    buttonShow,
+    button,
+    container: secondContainer,
+    condition: true
+  },
+
+  {
+    buttonShow,
+    button,
+    container: thirdContainer,
+    condition: true
+  }
+]
+
+for (let i = 0; i < arrayButton.length; i++) {
+  arrayButton[i].buttonShow = buttonShow[i]
+  arrayButton[i].button = button[i]
 }
 
-var buttonClick = function buttonClick(object) {
+let buttonClick = function (object) {
   object.button.addEventListener('click', function () {
     if (object.condition) {
-      object.container.classList.add('container--expand');
-      object.button.value = 'Скрыть';
-      object.buttonShow.classList.add('show-all--active');
-      object.condition = false;
+      object.container.classList.add('container--expand')
+      object.button.value = 'Скрыть'
+      object.buttonShow.classList.add('show-all--active')
+      object.condition = false
     } else {
-      object.container.classList.remove('container--expand');
-      object.button.value = 'Показать все';
-      object.buttonShow.classList.remove('show-all--active');
-      object.condition = true;
+      object.container.classList.remove('container--expand')
+      object.button.value = 'Показать все'
+      object.buttonShow.classList.remove('show-all--active')
+      object.condition = true
     }
-  });
-};
-
-for (var _i2 = 0; _i2 < arrayButton.length; _i2++) {
-  buttonClick(arrayButton[_i2]);
+  })
 }
 
-var openBurger = document.querySelector('.burger-menu');
-var body = document.querySelector('.body');
-var mainMenu = document.querySelector('.container__wrapper-menu');
-var requestCall = document.getElementsByClassName('phone');
-var call = document.querySelector('.container__wrapper-call');
-var feedbackSend = document.getElementsByClassName('chat');
-var feedback = document.querySelector('.container__wrapper-feedback');
-var unlock = true;
+for (let i = 0; i < arrayButton.length; i++) {
+  buttonClick(arrayButton[i])
+}
+
+const openBurger = document.querySelector('.burger-menu')
+const body = document.querySelector('.body')
+const mainMenu = document.querySelector('.container__wrapper-menu')
+let requestCall = document.getElementsByClassName('phone')
+const call = document.querySelector('.container__wrapper-call')
+let feedbackSend = document.getElementsByClassName('chat')
+const feedback = document.querySelector('.container__wrapper-feedback')
+
+let unlock = true
 
 if (openBurger) {
   openBurger.addEventListener('click', function () {
-    popupOpen(mainMenu);
-  });
+    popupOpen(mainMenu)
+  })
 }
 
 if (requestCall.length > 0) {
-  for (var _i3 = 0; _i3 < requestCall.length; _i3++) {
-    var _button = requestCall[_i3];
-
-    _button.addEventListener('click', function () {
-      popupOpen(call);
-    });
+  for (let i = 0; i < requestCall.length; i++) {
+    let button = requestCall[i]
+    button.addEventListener('click', function () {
+      popupOpen(call)
+    })
   }
 }
 
 if (feedbackSend.length > 0) {
-  for (var _i4 = 0; _i4 < feedbackSend.length; _i4++) {
-    var _button2 = feedbackSend[_i4];
-
-    _button2.addEventListener('click', function () {
-      popupOpen(feedback);
-    });
+  for (let i = 0; i < feedbackSend.length; i++) {
+    let button = feedbackSend[i]
+    button.addEventListener('click', function () {
+      popupOpen(feedback)
+    })
   }
 }
 
-var popupCloseIcon = document.getElementsByClassName('popup-close');
+let popupCloseIcon = document.getElementsByClassName('popup-close')
 
 if (popupCloseIcon.length > 0) {
-  var _loop = function _loop(_i5) {
-    var elem = popupCloseIcon[_i5];
+  for (let i = 0; i < popupCloseIcon.length; i++) {
+    const elem = popupCloseIcon[i]
     elem.addEventListener('click', function () {
-      popupClose(elem.closest('.popup'), unlock);
-    });
-  };
-
-  for (var _i5 = 0; _i5 < popupCloseIcon.length; _i5++) {
-    _loop(_i5);
+      popupClose(elem.closest('.popup'), unlock)
+    })
   }
 }
 
 function popupOpen(popupName) {
   if (popupName) {
-    var popupActive = document.querySelector('.popup-open');
-
+    const popupActive = document.querySelector('.popup-open')
     if (popupActive) {
-      popupClose(popupActive);
+      popupClose(popupActive)
     }
-
-    bodyLock();
-    popupName.classList.add('popup-open');
+    bodyLock()
+    popupName.classList.add('popup-open')
     popupName.addEventListener('click', function (evt) {
       if (!evt.target.closest('.popup-content')) {
-        popupClose(evt.target.closest('.popup'));
+        popupClose(evt.target.closest('.popup'))
       }
-    });
+    })
   }
 }
 
 function popupClose(popupActive, unlock) {
   if (popupActive) {
-    popupActive.classList.remove('popup-open');
+    popupActive.classList.remove('popup-open')
   }
-
   if (!unlock) {
-    bodyUnlock();
+    bodyUnlock()
   }
 }
 
 function bodyLock() {
-  body.classList.add('lock');
-  unlock = false;
+  body.classList.add('lock')
+  unlock = false
 }
 
 function bodyUnlock() {
-  body.classList.remove('lock');
-  unlock = true;
+  body.classList.remove('lock')
+  unlock = true
 }
 
-;
-
-(function () {
+;(function () {
   if (!Element.prototype.closest) {
     Element.prototype.closest = function (css) {
-      var node = this;
-
+      var node = this
       while (node) {
-        if (node.matches(css)) return node;else node = node.parentElement;
+        if (node.matches(css)) return node
+        else node = node.parentElement
       }
-
-      return null;
-    };
+      return null
+    }
   }
-})();
-
-(function () {
+})()
+;(function () {
   if (!Element.prototype.matches) {
-    Element.prototype.matches = Element.prototype.matchesSelector || Element.prototype.webkitMatchesSelector || Element.prototype.mozMatchesSelector || Element.prototype.msMatchesSelector;
+    Element.prototype.matches =
+      Element.prototype.matchesSelector ||
+      Element.prototype.webkitMatchesSelector ||
+      Element.prototype.mozMatchesSelector ||
+      Element.prototype.msMatchesSelector
   }
-})();
+})()
 
 /***/ }),
 
